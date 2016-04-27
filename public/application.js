c7e401dd39ea6fbe615b19d378868b6c20dabcec
Tri Han <trihan51@yahoo.com>
2016-04-27 12:21:13 -0700

    added ngRoute to app

--- public/application.js
+++ public/application.js
@@ -1,6 +1,12 @@
 var mainApplicationModuleName = 'game-on-web';
 
-var mainApplicationModule = angular.module(mainApplicationModuleName, ['home']);
+var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngRoute', 'home']);
+
+mainApplicationModule.config(['$locationProvider',
+	function($locationProvider) {
+		$locationProvider.hashPrefix('!');
+	}
+]);
 
 angular.element(document).ready(function() {
 	angular.bootstrap(document, [mainApplicationModuleName]);
---
+++ public/home/config/home.client.routes.js
@@ -0,0 +1,11 @@
+angular.module('home').config(['$routeProvider',
+	function($routeProvider) {
+		$routeProvider.
+		when('/', {
+			templateUrl: 'home/views/home.client.view.html'
+		}).
+		otherwise({
+			redirectTo: '/'
+		});
+	}
+]);
